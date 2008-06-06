a070d8e9a9859976c2999a36cbe42b8807936fcc
Kai Hendry <hendry@iki.fi>
2008-06-06 22:59:29 +0100

    Refactored

--- content/wc.js
+++ content/wc.js
@@ -8,18 +8,15 @@ function tabRemoved(event)
     // If there are two tabs, the second tab has no title and the closed tab
     // does have a title (ie is not the same tab) then close the browser
 
-	if ((num == 2) && (gBrowser.getBrowserAtIndex(1).contentTitle == '') && (event.target.linkedBrowser.contentTitle != '')) {
-
-		goQuitApplication();
-
-	}
+	if ((num == 2) && (! gBrowser.getBrowserAtIndex(1).contentTitle) && event.target.linkedBrowser.contentTitle)
+	{ goQuitApplication(); }
 
 }
 
 function webcRestart() {
 
-    	var beforeElement = document.getElementById("search-container");
-    	beforeElement.parentNode.insertItem("restartbrowser-button", beforeElement);
+    	// var beforeElement = document.getElementById("search-container");
+    	// beforeElement.parentNode.insertItem("restartbrowser-button", beforeElement);
     	document.persist("nav-bar", "currentset");
     	// Add close tab listener, gBrowser has not been initiated by this point
 	    getBrowser().tabContainer.addEventListener("TabClose", tabRemoved, false);
