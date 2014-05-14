eaca1d37785f48d6c706a459ce4f666a60847464
Kai Hendry <hendry@webconverger.com>
2014-05-14 16:43:29 +0800

    Work to make chrome=webconverger work with showprintbutton http://s.natalian.org/2014-05-14/showprintbutton-webconverger.png

--- src/webconverger.js
+++ src/webconverger.js
@@ -62,6 +62,29 @@ function BrowserLoadURL(aTriggeringEvent, aPostData) { // override browser.js
 	focusElement(content);
 }
 
+
+(function() {
+	function startup() {
+		var navigatorToolbox = document.getElementById("navigator-toolbox");
+		navigatorToolbox.iconsize = "small";
+		navigatorToolbox.setAttribute("iconsize", "small");
+		var showPrintButton = false;
+		try {
+			showPrintButton = Services.prefs.getBoolPref("extensions.webconverger.showprintbutton");
+		} catch (e) {}
+		if (showPrintButton) {
+			document.getElementById("wc-print").removeAttribute("hidden");
+		}
+		window.removeEventListener("load", startup, false);
+	}
+
+	function shutdown() {
+		window.removeEventListener("unload", shutdown, false);
+	}
+
+	window.addEventListener("load", startup, false);
+	window.addEventListener("unload", shutdown, false);
+})();
 // Disable shift click from opening window
 // Fixes https://github.com/Webconverger/webconverger-addon/issues/18
 var ffWhereToOpenLink = whereToOpenLink;
