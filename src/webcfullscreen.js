24177bf265b61dc63f0a878735cd0bda352ea59c
Michael Kaply <consulting@kaply.com>
2014-10-12 07:28:04 -0500

    Allow for switching of tabs automatically

--- src/webcfullscreen.js
+++ src/webcfullscreen.js
@@ -1,5 +1,6 @@
 (function() {
 	function startup() {
+		window.removeEventListener("load", startup, false);
 		var navigatorToolbox = document.getElementById("navigator-toolbox");
 		navigatorToolbox.iconsize = "small";
 		navigatorToolbox.setAttribute("iconsize", "small");
@@ -7,7 +8,26 @@
 		reloadButton.style.visibility = "visible";
 		var stopButton = document.getElementById("stop-button");
 		stopButton.style.visibility = "visible";
-		window.removeEventListener("load", startup, false);
+
+    var tabSwitchInterval = 0;
+    try {
+        tabSwitchInterval = Services.prefs.getIntPref("extensions.webconverger.tabswitchinterval");
+    } catch (e) {}
+    if (tabSwitchInterval > 0) {
+      window.setInterval(function() {
+        var visibleTabs = gBrowser.visibleTabs;
+        if (visibleTabs.length == 1) {
+          // Don't do anything if there is only one tab
+          return;
+        }
+        var selectedIndex = visibleTabs.indexOf(gBrowser.selectedTab);
+        selectedIndex = selectedIndex +1;
+        if (selectedIndex == visibleTabs.length) {
+          selectedIndex = 0;
+        }
+        gBrowser.selectTabAtIndex(selectedIndex);
+      }, tabSwitchInterval);
+    }
 	}
 
 	function shutdown() {
