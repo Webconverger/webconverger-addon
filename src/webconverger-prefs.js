2c5f0f54debe67d64112f76d7f1289e9b9fee993
Kai Hendry <hendry@webconverger.com>
2015-08-27 15:45:28 +0800

    datareporting.policy.dataSubmissionPolicyBypassNotification

--- src/webconverger-prefs.js
+++ src/webconverger-prefs.js
@@ -62,3 +62,5 @@ pref("plugin.default.state", 2);
 pref("browser.pocket.enabled", false);
 // Disable reader view function
 pref("reader.parse-on-load.enabled", false);
+
+pref("datareporting.policy.dataSubmissionPolicyBypassNotification", true);
